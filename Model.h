907480f648d801d37e7397a56257919ae93248cd
KutoKenta <58650784+KutoKenta@users.noreply.github.com>
2020-02-18 01:00:37 -0800

    Create Model.h

---
+++ Model.h
@@ -0,0 +1,13 @@
+// This is a standard TensorFlow Lite model file that has been converted into a
+// C data array, so it can be easily compiled into a binary for devices that
+// don't have a file system. It was created using the command:
+// xxd -i sine_model.tflite > sine_model_data.cc
+
+#ifndef MODEL_H_
+#define MODEL_H_
+
+extern const unsigned char train_model[];
+extern const int train_model_len;
+
+#endif 
+
